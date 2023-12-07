3ad43ba76be5ce0fd180258421c24f035a1c83e0
midiareshadi <midia.reshadi@gmail.com>
2023-12-07 11:23:38 +0000

    updating sparse matrix generator

--- testbench_File_Gen/sparse_generator/sparse_tb_gen.py
+++ testbench_File_Gen/sparse_generator/sparse_tb_gen.py
@@ -10,7 +10,7 @@ from scipy import sparse
 from scipy.sparse import csr_matrix
 from numpy.random import default_rng
 
-
+# Sparse matrix generator function
 def sparse_matrix_gen(m,n, density,seed):
 	np.random.seed(seed)
 	data_rvs = lambda n: np.random.randint(low=0, high=8, size=n, dtype=np.uint8)
@@ -18,11 +18,18 @@ def sparse_matrix_gen(m,n, density,seed):
 	sparse_gen_mat=sparse_gen_mat.toarray()
 	return sparse_gen_mat
 	
+# Matrix numpy matmul function
 def matmul (A , B):
     C = np.dot(A,B)
     return C
 
-#Opening json file:
+# Matrix heatmap generator function
+def heat_Func (matrix,index):
+	plt.spy(matrix, markersize=0.5)
+	plt.savefig(os.path.join('MatHeatMap/', f'{index}heatmap.png'), dpi=50)
+	plt.close()
+
+# Opening json file:
 with open('input_values.json', 'r') as f:
     data = json.load(f)
 
@@ -35,7 +42,7 @@ HeatmapGen = data ['heatmap_generation']
 rows = int(rows)
 cols = int(cols)
 density = float (density)
-
+HeatmapGen= bool (HeatmapGen)
 
 if HeatmapGen is True:
 	print ('Genetares input matrices for a', rows, 'by', cols, \
@@ -43,8 +50,6 @@ if HeatmapGen is True:
 else:
 	print ('Genetares input matrices for a', rows, 'by', cols, \
 	'systolic array with density = ', density, ' _without_ matrix heatmap generation' )
-
-
 
 # Generating sparse matrices
 A=sparse_matrix_gen(rows,cols,density,12)
@@ -57,26 +62,17 @@ print ("\n***** matrix B *****\n")
 np.savetxt(sys.stdout, B, fmt='%d', delimiter=' ')
 
 # Store matrices A and B in usual format
-np.savetxt('A.txt', A, fmt='%d')
-np.savetxt('B.txt', B, fmt='%d')
+np.savetxt(os.path.join('Reg_Format/', 'A.txt'), A, fmt='%d')
+np.savetxt(os.path.join('Reg_Format/', 'B.txt'), B, fmt='%d')
 
-if HeatmapGen is True:
-	HeatGen_Func(A)
-	HeatGen_Func(B)
-
-if HeatmapGen is True:
-	# A and Matrices heatmaps
-	plt.spy(A, markersize=0.5)
-	plt.savefig('A_heatmap.png', dpi=50)
-	plt.close()
-	#plt.show()
-	plt.spy(B, markersize=0.5)
-	plt.savefig('B_heatmap.png', dpi=50)
-	plt.close()
+# Heatmap generation function call
+if HeatmapGen is True :
+	heat_Func (A,'A_')
+	heat_Func (B,'B_')
 
 # Loading matrices
-A_mat = np.loadtxt('A.txt')
-B_mat = np.loadtxt('B.txt')
+A_mat = np.loadtxt(os.path.join('Reg_Format/', 'A.txt'))
+B_mat = np.loadtxt(os.path.join('Reg_Format/', 'B.txt'))
 
 # Function calls
 C= matmul (A , B)
@@ -91,6 +87,6 @@ np.savetxt(sys.stdout, B_mat, fmt='%d', delimiter=' ')
 print ("\n**********************\n")
 
 # Saving matrices
-np.savetxt('A_TB.txt', A_mat, fmt='%d')
-np.savetxt('B_TB.txt', B_mat, fmt='%d')
-np.savetxt('C_matmul.txt', C, fmt='%d')
+np.savetxt(os.path.join('TB_Format/', 'A_TB.txt'), A_mat, fmt='%d')
+np.savetxt(os.path.join('TB_Format/', 'B_TB.txt'), B_mat, fmt='%d')
+np.savetxt(os.path.join('Reg_Format/', 'C_matmul.txt'), C, fmt='%d')
