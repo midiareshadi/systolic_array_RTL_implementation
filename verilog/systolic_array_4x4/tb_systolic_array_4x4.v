4b46c425311c0c6b43982be9e48b713ea7e310b8
reshadim <reshadim@tcd.ie>
2023-12-18 17:12:01 +0000

    verilog 4x4 update

--- verilog/systolic_array_4x4/tb_systolic_array_4x4.v
+++ verilog/systolic_array_4x4/tb_systolic_array_4x4.v
@@ -4,24 +4,23 @@ module tb_systolic_array_4x4;
 
 parameter DATA_WIDTH=16,
 		period=10,
-		ld_time=(60*period),
-		start_ld=period,
-		sys_rst=ld_time+5,
-		bf_out_period=(15*period),
-		num_of_clk=100;
+		start_ld=15,
+		sys_rst=15,
+		num_of_clk=30;
 
 	
-	wire [(2*DATA_WIDTH)-1:0] 	tb_RD0, tb_RD1, tb_RD2, tb_RD3,
-								tb_GD0, tb_GD1, tb_GD2, tb_GD3;
-
-	wire [DATA_WIDTH-1:0] tb_FDi0, tb_FDi4, tb_FDi8, tb_FDi12;
-	wire [DATA_WIDTH-1:0] tb_FDo_0, tb_FDo_4, tb_FDo_8, tb_FDo_12;
+	
+	reg [DATA_WIDTH-1:0] tb_FDi0, tb_FDi4, tb_FDi8, tb_FDi12;
+	reg [(2*DATA_WIDTH)-1:0] tb_RD0, tb_RD1, tb_RD2, tb_RD3;
+	
+	wire [DATA_WIDTH-1:0] tb_FDo0, tb_FDo4, tb_FDo8, tb_FDo12;
+	wire [(2*DATA_WIDTH)-1:0] tb_GD0, tb_GD1, tb_GD2, tb_GD3;
 	
 	reg tb_clk, tb_rst, tb_result_ld;
 	
-	integer inFromFile_A,inFromFile_B, A_file,B_file;
+	integer inFromFile_A, inFromFile_B, A_file, B_file;
 	
-	systolic_array_4x4 systolic_array_4x4_inst(
+	systolic_array_4x4 sa_4x4_inst(
 		.clk(tb_clk),
 		.rst(tb_rst),
 		.result_ld(tb_result_ld),
@@ -33,6 +32,10 @@ parameter DATA_WIDTH=16,
 		.FDi4(tb_FDi4),
 		.FDi8(tb_FDi8),
 		.FDi12(tb_FDi12),
+		.FDo0(tb_FDo0),
+		.FDo4(tb_FDo4),
+		.FDo8(tb_FDo8),
+		.FDo12(tb_FDo12),
 		.sa_GD0(tb_GD0),
 		.sa_GD1(tb_GD1),
 		.sa_GD2(tb_GD2),
@@ -42,15 +45,15 @@ parameter DATA_WIDTH=16,
 // Reading B File
 initial begin
 	
-#start_ld; //according to the text bench of buffer B
+#start_ld;
               
   	B_file=$fopen("B_TB.txt","r");
       
 		while (! $feof(B_file)) 
 			begin         
-			inFromFile_B= $fscanf(B_file,"%d %d %d %d\n",tb_RD0, tb_RD1, tb_RD2, tb_RD3);
-			#(2*period);                         
-		end
+				inFromFile_B= $fscanf(B_file,"%d %d %d %d\n", tb_RD0, tb_RD1, tb_RD2, tb_RD3);
+				#(2*period);                         
+			end
                   
 	$fclose(B_file);
       
@@ -60,25 +63,25 @@ end // End of Reading B
 initial begin
 	
 #start_ld;
+
 	A_file=$fopen("A_TB.txt","r");
       
 	while (! $feof(A_file)) 
 		begin         
-			inFromFile_A=$fscanf(A_file,"%d %d %d %d \n",tb_FDi0, tb_FDi4, tb_FDi8, tb_FDi12);
+			inFromFile_A=$fscanf(A_file,"%d %d %d %d\n",tb_FDi0, tb_FDi4, tb_FDi8, tb_FDi12);
 			#(2*period);
-	end
+		end
                   
 	$fclose(A_file);
       
 end // End of input file
 
 initial begin // rst 
-	tb_rst <= 0;
+	tb_rst <= 1;
 	tb_clk <= 0;
 	#(sys_rst);
-	tb_rst <= 1;
-	#(period);
 	tb_rst <= 0;
+
 end
 
 initial begin // clock
@@ -89,7 +92,8 @@ end
 
 initial begin // result_ld
 
-	#(ld_time + bf_out_period+75);
+	tb_result_ld=0;
+	#(223);
 	tb_result_ld=1;
 	#(2*period);
 	tb_result_ld=0;
@@ -97,8 +101,8 @@ initial begin // result_ld
 end
 	
 initial begin
-	$dumpfile("VCD_wave.vcd");
-	$dumpvars; //(0, sys_array_tb);
+	$dumpfile("VCD_FILE.vcd");
+	$dumpvars;
 end
 			      
 endmodule
