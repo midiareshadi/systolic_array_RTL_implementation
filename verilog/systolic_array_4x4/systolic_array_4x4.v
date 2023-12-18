c7b6edcd3e09b983e5469404ec1fec9008af0ad4
midiareshadi <midia.reshadi@gmail.com>
2023-12-18 14:28:06 +0000

    updating 4x4 verilog code

--- verilog/systolic_array_4x4/systolic_array_4x4.v
+++ verilog/systolic_array_4x4/systolic_array_4x4.v
@@ -3,7 +3,7 @@ module systolic_array_4x4(
 		      FDi0, FDi4, FDi8, FDi12,
 		      RD0, RD1, RD2, RD3,
 		      clk, rst, result_ld,
-		      FDo_0, FDo_4, FDo_8, FDo_12,
+		      FDo0, FDo4, FDo8, FDo12,
 		      sa_GD0, sa_GD1, sa_GD2, sa_GD3);
 	parameter DATA_WIDTH=16;
 	
@@ -12,7 +12,7 @@ module systolic_array_4x4(
 	input clk, rst, result_ld;
 	
 	output [(2*DATA_WIDTH)-1:0] sa_GD0, sa_GD1, sa_GD2, sa_GD3;
-	output [DATA_WIDTH-1:0] FDo_0, FDo_4, FDo_8, FDo_12;
+	output [DATA_WIDTH-1:0] FDo0, FDo4, FDo8, FDo12;
 	
 	
 	wire [(2*DATA_WIDTH)-1:0] result[0:15];
@@ -23,28 +23,28 @@ module systolic_array_4x4(
 	
 
 	//from north and west
-	fsm_pe P0 (RD0, FDi0, clk, rst, GD[0], FDo_0, result[0], result_ld);
+	fsm_pe P0 (RD0, FDi0, clk, rst, GD[0], FDo0, result[0], result_ld);
 	//from north
-	fsm_pe P1 (RD1, FDo_0, clk, rst, GD[1], FDo[1], result[1], result_ld);
+	fsm_pe P1 (RD1, FDo0, clk, rst, GD[1], FDo[1], result[1], result_ld);
 	fsm_pe P2 (RD2, FDo[1], clk, rst, GD[2], FDo[2], result[2], result_ld);
 	fsm_pe P3 (RD3, FDo[2], clk, rst, GD[3], FDo[3], result[3], result_ld);
 	
 	//from west
-	fsm_pe P4 (GD[0], FDi4, clk, rst, GD[4], FDo_4, result[4], result_ld);
-	fsm_pe P8 (GD[4], FDi8, clk, rst, GD[8], FDo_8, result[8], result_ld);
-	fsm_pe P12 (GD[8], FDi12, clk, rst, sa_GD0, FDo_12, result[12], result_ld);
+	fsm_pe P4 (GD[0], FDi4, clk, rst, GD[4], FDo4, result[4], result_ld);
+	fsm_pe P8 (GD[4], FDi8, clk, rst, GD[8], FDo8, result[8], result_ld);
+	fsm_pe P12 (GD[8], FDi12, clk, rst, sa_GD0, FDo12, result[12], result_ld);
 	
 	//no direct inputs
 	//second row
-	fsm_pe P5 (GD[1], FDo_4, clk, rst, GD[5], FDo[5], result[5], result_ld);
+	fsm_pe P5 (GD[1], FDo4, clk, rst, GD[5], FDo[5], result[5], result_ld);
 	fsm_pe P6 (GD[2], FDo[5], clk, rst, GD[6], FDo[6], result[6], result_ld);
 	fsm_pe P7 (GD[3], FDo[6], clk, rst, GD[7], FDo[7], result[7], result_ld);
 	//third row
-	fsm_pe P9 (GD[5], FDo_8, clk, rst, GD[9], FDo[9], result[9], result_ld);
+	fsm_pe P9 (GD[5], FDo8, clk, rst, GD[9], FDo[9], result[9], result_ld);
 	fsm_pe P10 (GD[6], FDo[9], clk, rst, GD[10], FDo[10], result[10], result_ld);
 	fsm_pe P11 (GD[7], FDo[10], clk, rst, GD[11], FDo[11], result[11], result_ld);
 	//fourth row
-	fsm_pe P13 (GD[9], FDo_12, clk, rst, sa_GD1, FDo[13], result[13], result_ld);
+	fsm_pe P13 (GD[9], FDo12, clk, rst, sa_GD1, FDo[13], result[13], result_ld);
 	fsm_pe P14 (GD[10], FDo[13], clk, rst, sa_GD2, FDo[14], result[14], result_ld);
 	fsm_pe P15 (GD[11], FDo[14], clk, rst, sa_GD3, FDo[15], result[15], result_ld);
 	
--- verilog/systolic_array_4x4/tb_systolic_array_4x4.v
+++ verilog/systolic_array_4x4/tb_systolic_array_4x4.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ns
 
-module systolic_array_chip_4x4_tb;
+module tb_systolic_array_4x4;
 
 parameter DATA_WIDTH=16,
 		period=10,
@@ -15,17 +15,24 @@ parameter DATA_WIDTH=16,
 								tb_GD0, tb_GD1, tb_GD2, tb_GD3;
 
 	wire [DATA_WIDTH-1:0] tb_FDi0, tb_FDi4, tb_FDi8, tb_FDi12;
+	wire [DATA_WIDTH-1:0] tb_FDo_0, tb_FDo_4, tb_FDo_8, tb_FDo_12;
 	
 	reg tb_clk, tb_rst, tb_result_ld;
 	
 	integer inFromFile_A,inFromFile_B, A_file,B_file;
 	
-	systolic_array_chip_4x4 systolic_array_4x4(
-		.inA(tb_inA),
-		.inB(tb_inB),
+	systolic_array_4x4 systolic_array_4x4_inst(
 		.clk(tb_clk),
 		.rst(tb_rst),
 		.result_ld(tb_result_ld),
+		.RD0(tb_RD0),
+		.RD1(tb_RD1),
+		.RD2(tb_RD2),
+		.RD3(tb_RD3),
+		.FDi0(tb_FDi0),
+		.FDi4(tb_FDi4),
+		.FDi8(tb_FDi8),
+		.FDi12(tb_FDi12),
 		.sa_GD0(tb_GD0),
 		.sa_GD1(tb_GD1),
 		.sa_GD2(tb_GD2),
@@ -41,7 +48,7 @@ initial begin
       
 		while (! $feof(B_file)) 
 			begin         
-			inFromFile_B= $fscanf(B_file,"%d\n",tb_inB);
+			inFromFile_B= $fscanf(B_file,"%d %d %d %d\n",tb_RD0, tb_RD1, tb_RD2, tb_RD3);
 			#(2*period);                         
 		end
                   
@@ -57,7 +64,7 @@ initial begin
       
 	while (! $feof(A_file)) 
 		begin         
-			inFromFile_A=$fscanf(A_file,"%d\n",tb_inA);
+			inFromFile_A=$fscanf(A_file,"%d %d %d %d \n",tb_FDi0, tb_FDi4, tb_FDi8, tb_FDi12);
 			#(2*period);
 	end
                   
@@ -78,13 +85,6 @@ initial begin // clock
 	
 	repeat(num_of_clk)
 		#period tb_clk <= ~tb_clk;
-end
-
-initial begin // buffer read
-		
-	tb_buf_read=0;
-	#ld_time;
-	tb_buf_read=1;
 end
 
 initial begin // result_ld
